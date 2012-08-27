677fbf2ab6316b06cc245d8d2eff86a44e3ff430
serhiym <smoskov@softserveinc.com>
2012-08-27 14:06:41 +0300

    add new application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lmr::Application.config.secret_token = '8bef473aaf9ce3a31d0c348081592d92c6d1b2d1785b191fd451a7c3d366e5fd838cbb9514d146f22c781298b22cd3a9f0508104ec45d188c9a2d2af4c6a6dbe'
