a95fde9f68e3c2006bd5925efd22fd51c9cd1bee
serhiym <smoskov@softserveinc.com>
2012-08-27 17:38:54 +0300

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Lmr::Application.routes.draw do
+  devise_for :users, :skip => [:sessions]
+  as :user do
+    get 'login' => 'devise/sessions#new', :as => :new_user_session
+    post 'login' => 'devise/sessions#create', :as => :user_session
+    match 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session, :via => [:get, :delete]
+  end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
