c6a53871c5979017238cdf1769e816fc794ce55a
Igor Kasyanchuk <ikasyan@softserveinc.com>
2012-08-30 17:45:47 +0300

    added folders for deploy with dummy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,8 +46,8 @@ namespace :deploy do
 
   desc "Make sure there is something to deploy"
   task :check_revision, :roles => :web do
-    unless `git rev-parse HEAD` == `git rev-parse origin/master`
-      puts "WARNING: HEAD is not the same as origin/master"
+    unless `git rev-parse HEAD` == `git rev-parse origin/staging`
+      puts "WARNING: HEAD is not the same as origin/staging"
       puts "Run `git push` to sync changes."
       exit
     end
