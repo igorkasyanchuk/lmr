2cb1a013e4f05b0c64369bac7892c8d737dfb6b0
serhiym <smoskov@softserveinc.com>
2012-09-03 16:04:56 +0300

    change to new messages

--- app/decorators/helpers/forem/topics_helper_decorator.rb
+++ app/decorators/helpers/forem/topics_helper_decorator.rb
@@ -4,18 +4,4 @@ Forem::TopicsHelper.class_eval do
     text = "#{time_ago_in_words(post.created_at)} #{t("ago_by")} #{post.user.full_name}"
     link_to text, forem.forum_topic_path(post.topic.forum, post.topic, :anchor => "post-#{post.id}")
   end
-
-  def new_since_last_view_text(topic)
-    if forem_user
-      topic_view = topic.view_for(forem_user)
-      forum_view = topic.forum.view_for(forem_user)
-
-      if forum_view
-        if topic_view.nil? && topic.created_at > forum_view.past_viewed_at
-          content_tag :super, t('forum.new_messages')
-        end
-      end
-    end
-  end
-
 end
