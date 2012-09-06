09b48c5be0c049f5a6af0be93b4209b3966bed0a
serhiym <smoskov@softserveinc.com>
2012-09-06 12:54:32 +0300

    LMRUP-62, fix last response from hidden topic

--- app/decorators/models/forem/forum_decorator.rb
+++ app/decorators/models/forem/forum_decorator.rb
@@ -1,5 +1,11 @@
 Forem::Forum.class_eval do
+
+	def posts
+		super.joins(:topic).where('forem_topics.hidden = ?', false)
+	end
+
   def moderator?(user)
     user && user.content_manager? && !user.forum_blocked?
   end
+
 end
