3f9a7402032a35302b6e7608036a3a115f172d0a
Igor Kasyanchuk <ikasyan@softserveinc.com>
2012-09-07 13:49:55 +0300

    fixed page parts

--- app/models/page_part.rb
+++ app/models/page_part.rb
@@ -1,10 +1,22 @@
 class PagePart < ActiveRecord::Base
-  attr_accessible :content, :identifier, :format, :page_part
+  attr_accessible :content, :identifier, :format, :page_part, :plain_text, :html_text, :do_update
 
   FORMATS = ['html', 'text']
 
   validates_uniqueness_of :identifier
   validates_presence_of :identifier
+
+  attr_accessor :plain_text, :html_text, :do_update
+
+  before_save :update_content
+
+  def update_content
+    if self.format == 'text' 
+      self.content = self.plain_text
+    else
+      self.content = self.html_text
+    end if self.do_update.present?
+  end
 
   def self.[](identifier)
     page_part = PagePart.find_by_identifier(identifier)
