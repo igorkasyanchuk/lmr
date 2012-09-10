a456b4f5e4d442fa9f6e47c7ef43b2e42da9d680
serhiym <smoskov@softserveinc.com>
2012-09-10 17:59:16 +0300

    add quick fix

--- app/decorators/helpers/forem/topics_helper_decorator.rb
+++ app/decorators/helpers/forem/topics_helper_decorator.rb
@@ -1,7 +1,7 @@
 Forem::TopicsHelper.class_eval do
   def link_to_latest_post(topic)
     post = relevant_posts(topic).last
-    text = "#{time_ago_in_words(post.created_at)} #{t("ago_by")} #{post.user.try(:forem_name)}"
+    text = "#{time_ago_in_words(post.created_at)} #{t("ago_by")} #{post.user.try(:forum_name)}"
     link_to text, forem.forum_topic_path(post.topic.forum, post.topic, :anchor => "post-#{post.id}")
   end
 end
