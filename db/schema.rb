fa2137274faeefb59d802b4b97ae4af71e71bd18
serhiym <smoskov@softserveinc.com>
2012-09-10 17:24:47 +0300

    add forum name to user profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120910083759) do
+ActiveRecord::Schema.define(:version => 20120910093730) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -302,6 +302,7 @@ ActiveRecord::Schema.define(:version => 20120910083759) do
     t.integer  "role_id",                :default => 3
     t.boolean  "blocked",                :default => false
     t.string   "avatar"
+    t.string   "nickname"
   end
 
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
