96b4181630266ee1faafed52364fd953d1b87a41
serhiym <smoskov@softserveinc.com>
2012-09-25 14:16:25 +0300

    set assets compile false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Lmr::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -46,7 +46,7 @@ Lmr::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( jquery.js jquery-ui-autocomplete-min.js user.js admin/admin.js admin/admin.css forum.js forum.css dashboard/dashboard.css )
+  config.assets.precompile += %w( jquery.js jquery-ui-autocomplete-min.js user.js admin/admin.js forum.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
