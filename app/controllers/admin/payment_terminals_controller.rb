5b6e7ca719feba18f4b6d25a522381fa3a710342
azaytc <oleksiy.zaytsew@gmail.com>
2012-10-30 13:37:41 +0200

    add form for payment terminals

--- app/controllers/admin/payment_terminals_controller.rb
+++ app/controllers/admin/payment_terminals_controller.rb
@@ -1,4 +1,8 @@
 class Admin::PaymentTerminalsController <  Admin::DashboardController
   defaults :resource_class => PaymentTerminal, :collection_name => 'payment_terminals', :instance_name => 'payment_terminal'
  
+  def create
+    create! {admin_payment_terminals_path}
+  end
+
 end
--- app/models/terminal.rb
+++ app/models/terminal.rb
@@ -1,3 +1,3 @@
 class Terminal < PaymentTerminal 
-  
+
 end
