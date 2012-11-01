09767102b6209563e135e5b87b36727f78ea5a10
serhiym <smoskov@softserveinc.com>
2012-11-01 13:13:17 +0200

    refactoring conversation messages

--- app/controllers/dashboard/conversations_controller.rb
+++ app/controllers/dashboard/conversations_controller.rb
@@ -10,17 +10,11 @@ class Dashboard::ConversationsController < Dashboard::DashboardController
   end
 
   def create
-    body = params[:conversation][:body]
-    service_provider = params[:conversation][:service_provider_id]
-    subject = params[:conversation][:subject]
-    token = SecureRandom.hex(10)
-    @conversation = Conversation.new(user_id: current_user.id, service_provider_id: service_provider, token: token, subject: subject)
-    message = Message.create(body: body, from: current_user.full_name)    
-    if @conversation.save
-      @conversation.messages << message  
+    @conversation = Conversation.new(params[:conversation])
+    if current_user.conversations << @conversation
       redirect_to dashboard_conversation_url(@conversation)
     else
-      render :action => :new
+      render :new
     end
   end
 
@@ -30,15 +24,18 @@ class Dashboard::ConversationsController < Dashboard::DashboardController
   end
 
   def message
-    conversation = Conversation.find_by_id(params[:message][:conversation_id])
-    message = Message.new(body: params[:message][:body], from: current_user.full_name)    
-    if conversation.present? && message.save
-      conversation.messages << message
-      redirect_to dashboard_conversation_url(conversation)
+    params[:message][:from] = current_user.full_name
+    @message = Message.new(params[:message])
+    if @message.save
+      redirect_to dashboard_conversation_url(@message.conversation)
     else
-      redirect_to dashboard_conversations_url, :alert => 'Помилка при створенні повідомлення'
+      conversation = params[:message][:conversation_id]
+      if conversation
+        redirect_to dashboard_conversation_url(conversation), :alert => 'Помилка при створенні повідомлення'
+      else
+        redirect_to dashboard_conversations_url, :alert => 'Помилка при створенні повідомлення'
+      end
     end
   end
-
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ActiveRecord::Base
   attr_accessible :body, :conversation_id, :from
   belongs_to :conversation
+  validates_presence_of :body, :conversation_id, :from
 end
