fa36bf475bef6832a5c9b557da1245b4665123a9
serhiym <smoskov@softserveinc.com>
2012-11-08 16:34:49 +0200

    change UI of responsible persons, add validation on create responsible persons

--- app/models/responsible_person.rb
+++ app/models/responsible_person.rb
@@ -3,6 +3,7 @@ class ResponsiblePerson < ActiveRecord::Base
   belongs_to :service_provider
   self.table_name = 'responsible_persons'
 
+  validates_presence_of :first_name, :last_name
   attr_accessible :first_name, :middle_name, :last_name, :incumbency, :phone, :email
 
 
--- app/models/service_provider.rb
+++ app/models/service_provider.rb
@@ -3,6 +3,6 @@ class ServiceProvider < ActiveRecord::Base
   has_one :house
 
 
-  accepts_nested_attributes_for :responsible_persons
+  accepts_nested_attributes_for :responsible_persons, :allow_destroy => true
   attr_accessible :code, :names, :phone, :email, :address, :responsible_persons_attributes, :name, :district
 end
