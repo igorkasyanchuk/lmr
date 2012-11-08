4dca0d10695c7956960db8245ab2ddeffc49d4e9
serhiym <smoskov@softserveinc.com>
2012-11-08 17:24:08 +0200

    LMRUP-166, resolved, add validation

--- app/models/service_provider.rb
+++ app/models/service_provider.rb
@@ -4,6 +4,6 @@ class ServiceProvider < ActiveRecord::Base
   has_many :responsible_persons, :dependent => :destroy
   has_one :house
 
-  accepts_nested_attributes_for :responsible_persons, :allow_destroy => true
+  accepts_nested_attributes_for :responsible_persons, :reject_if => lambda { |a| a[:first_name].blank? && a[:last_name].blank? }, :allow_destroy => true
   
 end
