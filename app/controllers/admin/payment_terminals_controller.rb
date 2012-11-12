e2aec88ec5821e91c3489be42c6fec858d1a72d0
serhiym <smoskov@softserveinc.com>
2012-11-12 13:58:47 +0200

    LMRUP-171, fi crash on edit terminals

--- app/controllers/admin/payment_terminals_controller.rb
+++ app/controllers/admin/payment_terminals_controller.rb
@@ -2,7 +2,11 @@ class Admin::PaymentTerminalsController <  Admin::DashboardController
   defaults :resource_class => PaymentTerminal, :collection_name => 'payment_terminals', :instance_name => 'payment_terminal'
  
   def create
-    create! {admin_payment_terminals_path}
+    create! {admin_payment_terminals_url}
+  end
+
+  def update
+    update! {admin_payment_terminals_url}
   end
 
 end
