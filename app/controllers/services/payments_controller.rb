d84b1b68fe7a0110dc530a6af9117f90ca41fbe5
serhiym <smoskov@softserveinc.com>
2012-11-12 13:34:46 +0200

    fix terminal searches

--- app/controllers/services/payments_controller.rb
+++ app/controllers/services/payments_controller.rb
@@ -24,8 +24,7 @@ class Services::PaymentsController < ApplicationController
 
   def search
     location = prepare_coordinates(params[:query])
-    @departments = PaymentTerminal.within(5, :origin => location).all
-    @departments = PaymentTerminal.geo_scope(:origin => location).order("distance asc").limit(5) if @departments.blank?
+    @departments = PaymentTerminal.geo_scope(:origin => location).order("distance asc").limit(5)
     prepare_marker
   end
 
@@ -36,8 +35,8 @@ class Services::PaymentsController < ApplicationController
     end
 
     def prepare_coordinates query
-      location = Geocoder.search('Lviv, ' + query.to_s) 
-      location = location[0].geometry['location'].map {|k,v| v}
+      location = Geocoder.search('Львів, ' + query.to_s)
+      location = [location[0].latitude, location[0].longitude]
     end
 
 end
--- app/models/payment_terminal.rb
+++ app/models/payment_terminal.rb
@@ -1,9 +1,10 @@
 #encoding: utf-8
 class PaymentTerminal < ActiveRecord::Base
-  attr_accessible :code, :name, :address, :phone, :bank, :email, :payment_type, :type
+  attr_accessible :code, :name, :address, :phone, :bank, :email, :payment_type, :type, :latitude, :longitude
 
   acts_as_gmappable
-  acts_as_mappable :lat_column_name => :latitude,
+  acts_as_mappable :default_units => :kms,
+                   :lat_column_name => :latitude,
                    :lng_column_name => :longitude
 
   TERMINAL_TYPES = {
@@ -16,13 +17,22 @@ class PaymentTerminal < ActiveRecord::Base
   validates :type, :presence => true
   validates :code, :presence => true
   validates :address, :presence => true
+  validate :lat_lon_coordinates , :on => :create
 
   def gmaps4rails_address
-    "#{self.address}"
+    "Львів, #{self.address}"
   end
 
   def gmaps4rails_title
     "#{self.name}, #{self.address}"
+  end
+
+  private
+
+  def lat_lon_coordinates
+    unless self.latitude.present? && self.longitude.present?
+      errors.add :address, "неможливо знайти координати адреси"
+    end
   end
 
 
